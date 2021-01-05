bca29dc300abe136602f5eda66fbf9ab9a40d23e
SoorajSivaraman <75786247+SoorajSivaraman@users.noreply.github.com>
2021-01-05 18:11:28 +0530

    Add files via upload

--- paper.js
+++ paper.js
@@ -2,8 +2,8 @@ class Paper {
     constructor(x, y) {
       var options = {
           'isStatic' : false,
-          'restitution':0,
-          'friction':0,
+          'restitution':0.3,
+          'friction':0.5,
           'density':1.2
       }
       this.x = x;
